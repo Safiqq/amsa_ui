c894e028c13c6fcc71b490f92992a37d2b0d1ecb
ceavinrufus <18221162@std.stei.itb.ac.id>
2023-05-26 12:39:58 +0700

    coba

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -26,12 +26,12 @@ interface IRegistData {
   pekerjaan: string,
   bundle: number,
   kodeReferral: string,
-  buktiTransfer: string,
-  // buktiTransfer: {
-  //   file: string,
-  //   filename: string,
-  //   mimetype: string,
-  // },
+  // buktiTransfer: string,
+  buktiTransfer: {
+    file: string,
+    filename: string,
+    mimetype: string,
+  },
   namaAkunTransfer: string,
   bundleBuddies: IBuddiesData[],
   day: number,
@@ -47,12 +47,12 @@ export default function FormPage() {
     pekerjaan: '',
     bundle: 0,
     kodeReferral: '',
-    buktiTransfer: '',
-    // buktiTransfer: {
-    //   file: '',
-    //   filename: '',
-    //   mimetype: '',
-    // },
+    // buktiTransfer: '',
+    buktiTransfer: {
+      file: '',
+      filename: '',
+      mimetype: '',
+    },
     namaAkunTransfer: '',
     bundleBuddies: [],
     day: 0,
@@ -126,23 +126,23 @@ export default function FormPage() {
     setRegistData({ ...registData, bundleBuddies: newBundleBuddies });
   };
 
-  // const handleFileChange = (event: { target: { name: any; value: any, files: any; }; }) => {
-  //   const { name, value, files } = event.target;
-  //   if (files.length > 0) {
-  //     if ((files[0].size / 1024) <= 1024) {
-  //       let reader = new FileReader();
-  //       reader.readAsDataURL(files[0]);
-  //       reader.onload = function (e) {
-  //         if (e.target) {
-  //           setRegistData({ ...registData, [name]: { file: e.target.result, filename: value.split('\\').pop().split('/').pop(), mimetype: files[0].type } });
-  //         }
-  //       };
-  //     } else {
-  //       event.target.value = null;
-  //       handleModal(true, "error", "Maximum file size is 1MB!");
-  //     }
-  //   }
-  // }
+  const handleFileChange = (event: { target: { name: any; value: any, files: any; }; }) => {
+    const { name, value, files } = event.target;
+    if (files.length > 0) {
+      if ((files[0].size / 1024) <= 1024) {
+        let reader = new FileReader();
+        reader.readAsDataURL(files[0]);
+        reader.onload = function (e) {
+          if (e.target) {
+            setRegistData({ ...registData, [name]: { file: e.target.result, filename: value.split('\\').pop().split('/').pop(), mimetype: files[0].type } });
+          }
+        };
+      } else {
+        event.target.value = null;
+        handleModal(true, "error", "Maximum file size is 1MB!");
+      }
+    }
+  }
 
   const handleSubmit = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
@@ -469,14 +469,14 @@ export default function FormPage() {
                 </div>
                 <div className='opacity-60'>Input berupa link Google Drive</div>
                 <div className="mt-1">
-                  {/* <input name="buktiTransfer" type="file" accept="image/png, image/jpeg" required
+                  <input name="buktiTransfer" type="file" accept="image/png, image/jpeg" required
                     onChange={handleFileChange}
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  /> */}
-                  <input name="buktiTransfer" type="text" value={registData["buktiTransfer"]} required
+                  />
+                  {/* <input name="buktiTransfer" type="text" value={registData["buktiTransfer"]} required
                     onChange={handleInputChange}
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
+                  /> */}
                 </div>
               </div>
 
